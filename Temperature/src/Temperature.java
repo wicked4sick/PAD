49b060b2a4407bd5172a5d7804387251c22c00d5
wicked4sick <sick.bernhard@gmail.com>
2015-10-21 22:06:19 +0200

    Initial commit
    After import of downloaded java-class.

---
+++ Temperature/src/Temperature.java
@@ -0,0 +1,36 @@
+
+// Temperature.java
+//
+// transforms Fahrenheit to Celsius
+
+
+// import Scanner class for command line reading
+import java.util.Scanner;
+
+public class Temperature {   
+   
+   public static void main(String[] args) {
+      
+      // variables for the problem
+      double fahrenheit, celsius;
+      
+      Scanner scan = new Scanner (System.in);
+      
+      System.out.println("Geben Sie eine Temperatur in Fahrenheit ein und drücken Sie die Enter-Taste");
+      
+      // read command line and interpret as Double
+      fahrenheit = scan.nextDouble();
+      
+             
+      // calculate celsius and round it to 1/100 degrees
+      celsius = 5.0 / 9 * (fahrenheit - 32);               
+      // use Math class for round
+      celsius = Math.round(celsius * 100);                
+      celsius = celsius / 100.0;                           
+      
+      
+      // output the temperature in celsius
+      System.out.println(fahrenheit + " Grad Fahrenheit entsprechen "       
+                   + celsius + " Grad Celsius.");
+   }
+}
